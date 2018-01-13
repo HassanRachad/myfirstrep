d04103d47d885eec39b6454533ad9cb420750f59
HassanRachad <h.baous@gmail.com>
2018-01-13 20:39:14 +0000

    commit first spring app

---
+++ src/main/java/com/myfirstrep/MyfirstrepApplication.java
@@ -0,0 +1,12 @@
+package com.myfirstrep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfirstrepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyfirstrepApplication.class, args);
+	}
+}
